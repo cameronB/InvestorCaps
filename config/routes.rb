b8fd0501b8bf8663af339823dc7d7dea37334c7c
Cameron Bradley <cameronbradley.git@gmail.com>
2012-10-31 21:24:01 +1100

    added tests, added full_title helper method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 InvestorCaps::Application.routes.draw do
+  resources :users
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
-  resources :users
+  get "static_pages/contact"
 end
