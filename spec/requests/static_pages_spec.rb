1d8ad2eaac91cc95a74fcffd9bcbce3c5131eec7
Cameron Bradley <cameronbradley.git@gmail.com>
2012-11-01 21:36:58 +1100

    basic user model created

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Welcome to InvestorCaps') }
+    it { should have_selector('h1', text: 'InvestorCaps') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
   end
@@ -15,21 +15,36 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('h1', text: 'About') }
     it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "InvestorCaps"
+    page.should have_selector 'h1', text: 'InvestorCaps'
   end
 end
