eb0c00a0cac07b4ffce5df7d1b305bf1f95b49c5
Cameron Bradley <cameronbradley.git@gmail.com>
2013-02-10 19:12:01 +1100

    minor comment test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+
+  #include the session helper
   include SessionsHelper
 
   #cross site request forgery (CSRF) - feature makes all generated forms have a hidden id.
