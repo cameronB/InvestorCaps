f7184018c1d2d44ce8c39ef1352f6d05aa6cfdba
Cameron Bradley <cameronbradley.git@gmail.com>
2013-02-17 11:32:10 +1100

    remove deleted files

--- db/migrate/20130213083233_create_votes.rb
+++
@@ -1,8 +0,0 @@
-class CreateVotes < ActiveRecord::Migration
-    create_table :votes do |t|
-      t.integer :user_id
-      t.integer :post_id
-
-      t.timestamps
-    end
-end
--- db/migrate/20130213084431_add_up_to_votes.rb
+++
@@ -1,5 +0,0 @@
-class AddUpToVotes < ActiveRecord::Migration
-  def change
-  	    add_column :votes, :up, :boolean
-  end
-end
