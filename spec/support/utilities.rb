be9e9c8d26ed83268b42470d8fda13bb34440e42
Cameron Bradley <cameronbradley.git@gmail.com>
2013-03-02 20:42:31 +1100

    passing suite attempt 2 heh..

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,8 @@
 include ApplicationHelper
+
+def sign_in(user)
+  visit signin_path
+  fill_in "user_email",    with: user.email
+  fill_in "user_password", with: user.password
+  click_button "Sign in"
+end
